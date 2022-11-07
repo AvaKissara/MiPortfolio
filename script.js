76d8472ee2ac050812b8a217bac88e3e457771be
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2022-11-07 20:08:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+/*  ======================= SCROLLING SECTIONS ======================= */
+$('a[href*=#]:not([href=#])').click(function() 
+{
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+
+
+/*  ======================= H1 EFFECT ======================= */
+var spanText = function spanText(text) 
+{
+    var string = text.innerText;
+    var spaned = '';
+    for (var i = 0; i < string.length; i++) {
+      if(string.substring(i, i + 1) === ' ') spaned += string.substring(i, i + 1);
+      else spaned += '<span>' + string.substring(i, i + 1) + '</span>';
+    }
+    text.innerHTML = spaned;
+}
+
+var headline = document.querySelector("h1");
+
+spanText(headline);
+
+let animations = document.querySelectorAll('.animation');
+
+animations.forEach(animation => 
+{
+    let letters = animation.querySelectorAll('span');
+    letters.forEach((letter, i) => {
+        letter.style.animationDelay = (i * 0.1) + 's';
+    })
+})
+
+
+
+
+/*  ======================= PANELS ======================= */
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() 
+{
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) 
+{
+  console.log(e.propertyName);
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+
